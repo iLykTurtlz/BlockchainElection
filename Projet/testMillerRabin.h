b59f652339ff6b742b0a43708a66ac02fe18ccf4
Margaux971 <99265418+Margaux971@users.noreply.github.com>
2022-03-09 10:22:14 +0100

    Create testMillerRabin.h

---
+++ Projet/testMillerRabin.h
@@ -0,0 +1,9 @@
+#ifndef TESTMILLERRABIN_H
+#define TESTMILLERRABIN_H
+
+int witness(long a, long b, long d, long p);
+long rand_long(long low, long up);
+int is_prime_miller(long p, int k);
+
+
+#endif
