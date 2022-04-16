a1e91f1f8f16ee42832dfed5ab667baaf9f25c92
iLykTurtlz <pjarski@sfcm.edu>
2022-04-16 12:53:17 +0200

    generationClefs

--- Projet/testGenerationClefs.c
+++ Projet/testGenerationClefs.c
@@ -8,19 +8,27 @@
 
 int main()  {
     srand(time(NULL));
+    int min_size = 3;
+    int max_size = 7;
 
     for (int i=0; i<10; i++)    {
-        long p,q,*n, long *s, long *u
+        long p,q,*n, *s, *u;
         long p,q,n,s,u;
 
         p = random_prime_number(min_size, max_size,5000);
         q = random_prime_number(min_size, max_size,5000);
-        while (p==q)    {
+        while (p==-1)   {
+            p = random_prime_number(min_size, max_size,5000);
+        }
+        while (p==q || q == -1)    {
             q = random_prime_number(min_size,max_size,5000);
         }
+        generate_key_values(p,q,n,s,u);
+
         fprintf(stderr,"Premier essai:\n");
         fprintf(stderr,"(s,n) = (%ld,%ld) \t (u,n) = (%ld,%ld)\n",s,n,u,n);
 
+        //pour avoir u positif
         if (u<0) {
             long t = (p-1)*(q-1);
             u = u+t; //on aura toujours s*u mod t = 1
