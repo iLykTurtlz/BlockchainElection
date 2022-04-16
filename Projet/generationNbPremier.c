ede33df8a013e37b45f8cfd45a9d2c648adfbd6d
iLykTurtlz <pjarski@sfcm.edu>
2022-04-16 13:13:46 +0200

    generationNbPremier random_prime_number - condition du while

--- Projet/generationNbPremier.c
+++ Projet/generationNbPremier.c
@@ -12,13 +12,10 @@ long random_prime_number(int min_size, int max_size, int k)  {
     long min_p = pow(2,(min_size));
     long max_p = pow(2,(max_size-1));
     long p = rand_long(min_p, max_p);
-    while (p < max_p)   {
-        if (is_prime_miller(p,k))   {
-            return p;
-        }
+    while (!is_prime_miller(p,k))   {
         p = rand_long(min_p, max_p);
     }
-    return -1;  //cas d'echec
+    return p;
 }
 
 /*
