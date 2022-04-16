169c10cc1bf219079ac9de73f4d9f5b74bb3cb10
PAUL <pjarski@sfcm.edu>
2022-04-16 10:49:56 +0200

    nouvelles signatures dans blockchain.h

--- Projet/blockchain.h
+++ Projet/blockchain.h
@@ -21,6 +21,7 @@ char *block_to_str(Block *block);
 unsigned char* hash_function_block(const char* str);
 int count_zeros(unsigned char* str);
 void compute_proof_of_work(Block *B, int d);
-
+int verify_block(Block *B, int d);
+void delete_block(Block *B);
 
 #endif
