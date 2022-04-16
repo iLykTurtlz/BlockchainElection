0eed9503c63189be9e964b86bf2c2cc7ab73ab2a
iLykTurtlz <pjarski@sfcm.edu>
2022-04-16 14:36:37 +0200

    free! test declaration

--- Projet/declaration.h
+++ Projet/declaration.h
@@ -14,5 +14,6 @@ Protected *init_protected(Key *pKey, char *mess, Signature *sgn);
 int verify(Protected *pr);
 char *protected_to_str(Protected *pr);
 Protected *str_to_protected(char *str);
+void freeProtected(Protected *pr);
 
 #endif
